36057e6d67c7c7d359dd8fddd58b00191b5d9ad1
YASH  LALPOTU <67863505+yashlalpotu11@users.noreply.github.com>
2021-07-01 11:21:28 +0530

    commit

---
+++ Arrays/max_ele.cpp
@@ -0,0 +1,30 @@
+
+/* Q) Given an array, find the largest element in it. 
+Input : arr[] = {20, 10, 20, 4, 100}
+Output : 100
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+const int m = 1e9+7;
+typedef long long int ll;
+
+int maxElement(int arr[], int n){
+
+    int max_ele = arr[0];
+
+    for(int i = 1; i < n; i++){
+        if(arr[i] >= max_ele){
+            max_ele = arr[i];
+        }
+    }
+    return max_ele;
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<maxElement(arr,n);
+
+}
