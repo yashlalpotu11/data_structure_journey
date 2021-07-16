92ef5536423740fc05314b88e851467119013b09
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-07-16 08:43:47 +0530

    commit

---
+++ Arrays/Fix_point.cpp
@@ -0,0 +1,39 @@
+
+/*
+Q) Find a Fixed Point (Value equal to index) in a given array
+
+Input: arr[] = {-10, -5, 0, 3, 7}
+Output: 3  // arr[3] == 3 
+
+*/
+
+// 1) Binary Search(Recursive)
+
+#include<bits/stdc++.h>
+using namespace std;
+const int m = 1e9+7;
+typedef long long int ll;
+
+int FixPoint(int arr[], int low, int high){
+    
+    if(low <= high){
+        //find mid point
+        int mid = (low + high)/2;
+
+        if(mid == arr[mid]){
+            return mid;
+        }
+        else if(mid > arr[mid]){
+            return FixPoint(arr, (mid+1), high);
+        }
+        else{
+            return FixPoint(arr, low, high-1);
+        }
+    }
+    return -1;
+}
+int main(){
+    int arr[] = {-10, -1, 0, 3, 10, 11, 30, 50, 100};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Fixed point :"<<FixPoint(arr, 0, n-1);
+}
