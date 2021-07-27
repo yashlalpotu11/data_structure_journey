93a692986dc8b677a3bcf8f45864ae2fb75b753d
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-07-28 00:54:15 +0530

    commit

---
+++ Arrays/range.cpp
@@ -0,0 +1,21 @@
+
+
+/*
+Q)Given N numbers and Q queries, each query consists of L and R. 
+Task is to write a program which prints the count of numbers which 
+divides all numbers in the given range L-R.
+
+Input : a = {3, 4, 2, 2, 4, 6} 
+        Q = 2
+        L = 1 R = 4  
+        L = 2 R = 6
+    
+Output :  0
+          2
+
+Explanation : The range 1-4 has {3, 4, 2, 2} 
+which does not have any number that divides all the 
+numbers in this range. 
+The range 2-6 has {4, 2, 2, 4, 6} which has  2 numbers {2, 2} which divides 
+all numbers in the given range. 
+*/
