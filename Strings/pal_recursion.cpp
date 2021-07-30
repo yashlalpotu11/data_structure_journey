c0a8d8f0da042c3d423160340f2aacdda9705825
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-07-30 14:12:31 +0530

    palindrome recursion

---
+++ Strings/pal_recursion.cpp
@@ -0,0 +1,30 @@
+
+/*
+Q) Check if string is palindrome or not(using recursion)
+
+input : abcba
+output : YES
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+const int m = 1e9 + 7;
+typedef long long int ll;
+
+bool ispal(string s, int l, int r){
+    if(l==r){
+        return true;
+    }
+    if(l > r){
+        return true;
+    }
+    if(s[l] != s[r]){
+        return false;
+    }
+    return ispal(s, l+1, r-1);
+}
+int main(){
+    string s; cin>>s;
+    int l = 0, r = s.length()-1;
+    ispal(s, l, r) ? cout<<"YES" : cout<<"NO";
+}
